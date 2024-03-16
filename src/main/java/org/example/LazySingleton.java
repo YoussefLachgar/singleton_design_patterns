762255e94397776459187e695b2b27714e4acd1f
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-03-16 12:48:05 +0000

    lazy singlton class completed

--- src/main/java/org/example/LazySingleton.java
+++ src/main/java/org/example/LazySingleton.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class LazySingleton {
+
+    private String[] LETERS = { "a", "b", "c", "d", "e"};
+
+    private List<String> data = Arrays.asList(LETERS);
+
+    private static LazySingleton lazyInstance;
+
+    private LazySingleton(){
+        Collections.shuffle(data);
+    }
+
+    public static LazySingleton getLazyInstance(){
+        if ( lazyInstance == null){
+            lazyInstance = new LazySingleton();
+        }
+        return lazyInstance;
+    }
+
+    public void printData(){
+        for (String item: data){
+            System.out.printf("%s ", item);
+        }
+        System.out.println();
+    }
 }
