cf2a693be1b8f29d2b03348d86fa33a2ad721314
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-03-16 12:49:18 +0000

    Main test completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LazySingleton instance1 = LazySingleton.getLazyInstance();
+        LazySingleton instance2 = LazySingleton.getLazyInstance();
+
+        System.out.println(instance1.hashCode());
+        instance1.printData();
+
+        System.out.println(instance2.hashCode());
+        instance2.printData();
     }
 }
